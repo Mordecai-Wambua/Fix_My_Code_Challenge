54a0ed880f7cd3eefeef61848f15fab10e8c1b46
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-15 11:08:04 +0300

    square and user fixes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Square class."""
+
 
 class square():
-    
+    """Actual class implementation."""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initializes all variables."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +17,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of the square."""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
