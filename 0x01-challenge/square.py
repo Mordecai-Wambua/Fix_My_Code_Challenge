b1f43bd267dde5390cd8ba256d3c5595f1799e70
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-15 11:15:05 +0300

    square and user fixes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Square class."""
 
 
-class square():
+class Square():
     """Actual class implementation."""
     width = 0
     height = 0
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
