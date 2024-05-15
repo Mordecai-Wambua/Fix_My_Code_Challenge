11e9ac43f09bdb4105ee6aac82cedcd2a5a92289
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-15 11:10:10 +0300

    square and user fixes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """Perimeter of the square."""
